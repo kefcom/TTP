b9b91fbe1dbe038995c0f49ea3551b1b9c873ece
kefcom <kefcom@hotmail.com>
2021-12-27 17:55:16 +0100

    Initial commit

---
+++ TTP/Form1.Designer.vb
@@ -0,0 +1,198 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.sp1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtFontSize = New System.Windows.Forms.TextBox()
+        Me.sndTmr = New System.Windows.Forms.Timer(Me.components)
+        Me.txtSpeed = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.log = New System.Windows.Forms.TextBox()
+        Me.getTweets = New System.Windows.Forms.Timer(Me.components)
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txt_hashtag = New System.Windows.Forms.TextBox()
+        Me.firstBoot = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(96, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "COM PORT:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(114, 16)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 26)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(16, 180)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(311, 46)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Connect"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(27, 68)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(81, 20)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Font Size:"
+        '
+        'txtFontSize
+        '
+        Me.txtFontSize.Location = New System.Drawing.Point(114, 62)
+        Me.txtFontSize.Name = "txtFontSize"
+        Me.txtFontSize.Size = New System.Drawing.Size(100, 26)
+        Me.txtFontSize.TabIndex = 5
+        Me.txtFontSize.Text = "2"
+        '
+        'sndTmr
+        '
+        '
+        'txtSpeed
+        '
+        Me.txtSpeed.Location = New System.Drawing.Point(114, 94)
+        Me.txtSpeed.Name = "txtSpeed"
+        Me.txtSpeed.Size = New System.Drawing.Size(100, 26)
+        Me.txtSpeed.TabIndex = 9
+        Me.txtSpeed.Text = "500"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 97)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(102, 20)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Send Speed:"
+        '
+        'log
+        '
+        Me.log.Location = New System.Drawing.Point(16, 249)
+        Me.log.Multiline = True
+        Me.log.Name = "log"
+        Me.log.Size = New System.Drawing.Size(1401, 961)
+        Me.log.TabIndex = 10
+        '
+        'getTweets
+        '
+        Me.getTweets.Interval = 5000
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(1546, 45)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(570, 426)
+        Me.WebBrowser1.TabIndex = 13
+        Me.WebBrowser1.Url = New System.Uri("", System.UriKind.Relative)
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 20
+        Me.ListBox1.Location = New System.Drawing.Point(1546, 506)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(1043, 704)
+        Me.ListBox1.TabIndex = 14
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(30, 136)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(78, 20)
+        Me.Label5.TabIndex = 16
+        Me.Label5.Text = "HashTag:"
+        '
+        'txt_hashtag
+        '
+        Me.txt_hashtag.Location = New System.Drawing.Point(114, 136)
+        Me.txt_hashtag.Name = "txt_hashtag"
+        Me.txt_hashtag.Size = New System.Drawing.Size(213, 26)
+        Me.txt_hashtag.TabIndex = 17
+        Me.txt_hashtag.Text = "dendermonde"
+        '
+        'firstBoot
+        '
+        Me.firstBoot.Interval = 10000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(2731, 1248)
+        Me.Controls.Add(Me.txt_hashtag)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.log)
+        Me.Controls.Add(Me.txtSpeed)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtFontSize)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents sp1 As IO.Ports.SerialPort
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtFontSize As TextBox
+    Friend WithEvents sndTmr As Timer
+    Friend WithEvents txtSpeed As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents log As TextBox
+    Friend WithEvents getTweets As Timer
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txt_hashtag As TextBox
+    Friend WithEvents firstBoot As Timer
+End Class
---
+++ TTP/Form1.vb
@@ -0,0 +1,187 @@
+﻿Public Class Form1
+    Dim buffer As String = ""
+    Dim currentPositionX = 0
+    Dim currentPositionY = 7500
+    Dim currentPositionY1 = 0
+    Dim maxWidth As Integer = 35
+    Dim maxHeight As Integer
+    Dim alphabet As String = "abcdefghijklmnopqrstuvwxyz1234567890!#@&_: "
+    Dim firstBoot1 As Boolean
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim found As Integer
+        If sp1.IsOpen = False Then
+            sp1.PortName = "COM" & TextBox1.Text
+            sp1.Close()
+            found = 0
+            Do
+                Try
+                    sp1.Open()
+                    found = 1
+                Catch ex As Exception
+                    TextBox1.Text = TextBox1.Text + 1
+                End Try
+            Loop Until found = 1 Or TextBox1.Text > 30
+            If found = 0 Then
+                MsgBox("Comport not found :'( are you sure it's connected?", MsgBoxStyle.Critical, "Fatal error")
+            Else
+                Button1.Enabled = False
+                sndTmr.Enabled = True
+            End If
+        End If
+
+        'init printer
+        maxWidth = (11000 / (txtFontSize.Text * 70)) + 5
+        maxHeight = (7500 / (txtFontSize.Text * 70)) + 3
+        'MsgBox(maxWidth)
+        'MsgBox(maxHeight)
+
+        sndTmr.Interval = txtSpeed.Text
+        resetPrinter()
+
+        firstBoot.Enabled = True
+
+    End Sub
+
+    Sub resetPrinter()
+        sendText("IN;")
+        sendText("SP1;")
+        sendText("PU" & currentPositionX & "," & currentPositionY & ";")
+        sendText("SI0." & txtFontSize.Text & ",0." & txtFontSize.Text & ";")
+        sendText("LB")
+        currentPositionX = 0
+        currentPositionY1 = 0
+        buffer = "Tweets containing #" & txt_hashtag.Text & ": " & vbCrLf
+        Application.DoEvents()
+        Threading.Thread.Sleep(5000)
+        sndTmr.Enabled = True
+    End Sub
+    Private Sub sndTmr_Tick(sender As Object, e As EventArgs) Handles sndTmr.Tick
+        If (buffer.Length > 0) Then
+            sendText(buffer.Substring(0, 1))
+            If (buffer.Substring(0, 1) = vbCrLf Or buffer.Substring(0, 1) = vbCr) Then
+                currentPositionX = 0
+                currentPositionY1 = currentPositionY1 + 1
+            Else
+                currentPositionX = currentPositionX + 1
+            End If
+            buffer = buffer.Substring(1, buffer.Length - 1)
+            Me.Text = currentPositionX & " / " & currentPositionY1 & "              Buffer length:" & buffer.Length
+            If currentPositionX = maxWidth Then
+                If currentPositionY1 < maxHeight Then
+                    currentPositionY1 = currentPositionY1 + 1
+                    currentPositionX = 0
+                    sendText(vbCr)
+                    sendText(vbCrLf)
+                Else
+                    sndTmr.Enabled = False
+                    If MsgBox("Please load new paper!", MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then
+                        currentPositionX = 0
+                        currentPositionY1 = 0
+                        resetPrinter()
+                    End If
+                End If
+            End If
+        End If
+    End Sub
+
+    Sub sendText(theText As String)
+        sp1.Write(theText)
+        If theText.Substring(theText.Length - 1, 1) = ";" And theText.Length > 1 Then
+            log.Text = log.Text & theText & vbCrLf
+        Else
+            log.Text = log.Text & theText
+        End If
+        log.SelectionStart = log.Text.Length
+        log.ScrollToCaret()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        firstBoot1 = False
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles getTweets.Tick
+        WebBrowser1.Navigate("https://nitter.net/search?f=tweets&q=%23" & txt_hashtag.Text & "&e-nativeretweets=on&e-replies=on&since=&until=&near=")
+    End Sub
+
+
+
+    Sub parseTweets()
+        Dim htdoc As HtmlDocument
+        htdoc = WebBrowser1.Document
+
+        Dim coll As New Collection
+        For Each elem As HtmlElement In htdoc.All
+            If elem.GetAttribute("className").ToLower.Split(" ").Contains("timeline-item") Then
+                coll.Add(elem)
+            End If
+        Next
+
+        If (coll.Count > 0) Then
+            For Each elem As HtmlElement In coll
+                Dim thisUserName As String = ""
+                Dim thisTweetText As String = ""
+                For Each child As HtmlElement In elem.All
+                    If child.GetAttribute("className").ToLower.Split(" ").Contains("username") Then
+                        thisUserName = child.InnerText.ToLower()
+                    End If
+                    If child.GetAttribute("className").ToLower.Split(" ").Contains("tweet-content") Then
+                        thisTweetText = child.InnerText.ToLower()
+                    End If
+                Next
+                If thisUserName <> "" Then
+                    Dim toAdd As String
+                    toAdd = thisUserName & ":" & thisTweetText.ToLower()
+                    toAdd = toAdd.Replace("#" & txt_hashtag.Text.ToLower(), "")
+                    toAdd = cleanText(toAdd)
+
+                    If toAdd.Length > maxWidth Then
+                        toAdd = toAdd.Substring(0, maxWidth - 6)
+                        toAdd = toAdd & "..."
+                    End If
+                    Dim found As Boolean
+                    found = False
+                    For Each boxElem As String In ListBox1.Items
+                        If boxElem = toAdd Then
+                            found = True
+                        End If
+                    Next
+                    If found = False Then
+                        ListBox1.Items.Add(toAdd)
+                        If firstBoot1 = False Then
+                            'only add to buffer after firstboot
+                            buffer = buffer & toAdd & vbCrLf
+                        End If
+
+                    End If
+                End If
+
+
+            Next
+            If (firstBoot1 = True) Then
+                firstBoot1 = False
+            End If
+        End If
+    End Sub
+
+    Function cleanText(theText As String)
+        For index As Integer = 1 To (theText.Length - 1)
+            If alphabet.Contains(theText.Substring(index, 1)) = False Then
+                theText = theText.Replace(theText.Substring(index, 1), "°")
+            End If
+        Next
+        theText = theText.Replace("°", "")
+        Return theText
+    End Function
+
+    Private Sub WebBrowser1_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
+        If (WebBrowser1.IsBusy = False) Then
+            parseTweets()
+        End If
+    End Sub
+    Private Sub firstBoot_Tick(sender As Object, e As EventArgs) Handles firstBoot.Tick
+        getTweets.Enabled = True
+        firstBoot.Enabled = False
+    End Sub
+End Class
